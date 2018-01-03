9a1c1d534230c78c4570ba8bd3854edeacfad1c6
andream16 <andrea.medda.uni@gmail.com>
2018-01-03 18:51:47 +0100

    Adding Currency.

--- model/currency.py
+++ model/currency.py
@@ -1,27 +1,61 @@
 from collections import namedtuple
-
 from dateutil import parser
-from pyspark import SparkContext
 from typing import List
 
-Currencies = namedtuple("Currency", "name date value")
+# 3rd party
+from pyspark import SparkContext
 
 
-def set_currency_table(sc: SparkContext, url: str):
+class CurrencyEntry:
 
-    df = sc.read.format("jdbc").options(
-        url=url,
-        driver='org.postgresql.Driver',
-        dbtable='currency',
-        user='postgres'
-    ).load()
-    df.createOrReplaceTempView("currency")
+    def __init__(self, date: str, value: float):
+        self.date = date
+        self.value = value
 
 
-def get_currency(sc: SparkContext) -> List :
-    df = sc.sql("Select name, date, value from currency where name = 'DOLLAR'")
-    currencies = []
-    curr = df.rdd.map(lambda row: Currencies(row[0], parser.parse(row[1]), row[2]))
-    for p in curr.collect():
-        currencies.append(p)
-    return currencies
+class Currency:
+
+    # DB
+    sql_entry = namedtuple("Currency", "name date value")
+    # Dictionary
+    currencies = {}
+
+    def __init__(self, name: str, entries: List[CurrencyEntry]):
+        self.name: str = name
+        self.entries: List[CurrencyEntry] = entries
+
+    # Setters
+    def set_currency_by_key_and_entries(self, key: str, entries: List[CurrencyEntry]):
+        if len(entries) > 0:
+            for entry in entries:
+                if key not in self.currencies:
+                    self.currencies[key] = [entry]
+                else:
+                    self.currencies[key].append(entry)
+
+    # Getters
+    def get_currencies(self, sc: SparkContext):
+        gpb_dollar_data_frame = self.get_currency_entries_by_key(sc, 'DOLLAR')
+        gpb_dollar_parsed_entries = gpb_dollar_data_frame.rdd.map(
+            lambda row: self.sql_entry(row[0], parser.parse(row[1]), row[2])).collect()
+        self.set_currency_by_key_and_entries('dollar', gpb_dollar_parsed_entries)
+        gpb_euro_data_frame = self.get_currency_entries_by_key(sc, 'EURO')
+        gpb_euro_parsed_entries = gpb_euro_data_frame.rdd.map(
+            lambda row: self.sql_entry(row[0], parser.parse(row[1]), row[2])).collect()
+        self.set_currency_by_key_and_entries('euro', gpb_euro_parsed_entries)
+        return self
+
+    # Helpers
+    @staticmethod
+    def get_currency_entries_by_key(sc: SparkContext, key: str):
+        return sc.sql('SELECT name, date, value FROM currency WHERE name = "{}"'.format(key))
+
+    @staticmethod
+    def set_currency_table(sc: SparkContext, url: str):
+        df = sc.read.format("jdbc").options(
+            url=url,
+            driver='org.postgresql.Driver',
+            dbtable='currency',
+            user='postgres'
+        ).load()
+        df.createOrReplaceTempView("currency")
