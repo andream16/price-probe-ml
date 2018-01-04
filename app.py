44223109e240d5fac9026b795f1312311a86730d
andream16 <andrea.medda.uni@gmail.com>
2018-01-04 14:13:52 +0100

    Refactoring app.py to be more general purpose.

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-# 3rd party
+# Spark
 from spark import spark
 
 # Configuration
 from configuration import configuration
 
-# Models
-from model import currency, category
+# Core
+from core import core
 
 app_configuration = configuration.get_configuration('remote')
-sc, url = spark.init_configuration(app_configuration)
-spark.init_tables(sc, url)
+spark = spark.Spark()
+spark_context = spark.init_configuration(app_configuration)
 
-category = category.Category("", [])
-category.get_categories_by_item("B0019X20R8", sc)
-currency = currency.Currency("", [])
-currency.get_currencies(sc)
+# Core Startup
+core.start_algorithm(spark_context)
+
+
