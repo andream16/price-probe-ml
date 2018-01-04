212fedd0b1e543b716072f8ec0bf5bf9733aa20f
andream16 <andrea.medda.uni@gmail.com>
2018-01-04 14:14:48 +0100

    Refactoring Spark class to also handle the creation of Temporary views.

--- spark/spark.py
+++ spark/spark.py
@@ -1,27 +1,43 @@
+# 3rd party
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
-from model import category, currency, item, manufacturer, price, review, trend
+
+TABLES = [
+    'category_item',
+    'currency',
+    'item',
+    'manufacturer',
+    'price',
+    'review',
+    'trend',
+]
 
 
-def init_configuration(app_configuration):
-    conf = SparkConf()
-    conf.setMaster("local[*]")
-    conf.setAppName("My application")
-    conf.set("spark.driver.extraClassPath", "configuration/postgresql-42.1.4.jar")
-    conf.set(" spark.executor.extraClassPath", "configuration/postgresql-42.1.4.jar")
-    conf.set("spark.sql.execution.arrow.enable", "true")
-    conf.set("spark.executor.memory", "1g")
-    sc = SparkContext(conf=conf)
-    sc_sql = SparkSession(sc)
-    url = app_configuration[2]+app_configuration[0]+":"+app_configuration[1]+'/priceprobe'
-    return sc_sql, url
+class Spark:
 
+    def init_configuration(self, configuration):
+        conf = SparkConf()
+        conf.setMaster("local[*]")
+        conf.setAppName("My application")
+        conf.set("spark.driver.extraClassPath", "configuration/postgresql-42.1.4.jar")
+        conf.set(" spark.executor.extraClassPath", "configuration/postgresql-42.1.4.jar")
+        conf.set("spark.sql.execution.arrow.enable", "true")
+        conf.set("spark.executor.memory", "1g")
+        sc = SparkSession(SparkContext(conf=conf))
+        url = configuration[2]+configuration[0]+":"+configuration[1]+'/priceprobe'
+        self.init_tables(sc, url)
+        return sc
 
-def init_tables(sc, url):
-    item.set_item_table(sc, url)
-    price.set_price_table(sc, url)
-    category.Category.set_category_item_table(sc, url)
-    currency.Currency.set_currency_table(sc, url)
-    manufacturer.set_manufacturer_table(sc, url)
-    review.set_review_table(sc, url)
-    trend.set_trend_table(sc, url)
+    def init_tables(self, sc, url):
+        for table in TABLES:
+            self.set_table_by_name(sc, url, table)
+
+    @staticmethod
+    def set_table_by_name(sc: SparkContext, url: str, table_name: str):
+        df = sc.read.format("jdbc").options(
+            url=url,
+            driver='org.postgresql.Driver',
+            dbtable=table_name,
+            user='postgres'
+        ).load()
+        df.createOrReplaceTempView(table_name)
